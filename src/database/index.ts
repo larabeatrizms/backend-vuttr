53f0bc0e51d1689211161cce9b5e14a80f1c1d62
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-14 02:48:47 -0300

    Procfile

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,44 +3,44 @@
 
 // createConnection();
 
-import {
-  getConnectionOptions,
-  ConnectionOptions,
-  createConnection,
-} from 'typeorm';
+// import {
+//   getConnectionOptions,
+//   ConnectionOptions,
+//   createConnection,
+// } from 'typeorm';
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-const getOptions = async () => {
-  let connectionOptions: ConnectionOptions;
-  connectionOptions = {
-    type: 'postgres',
-    synchronize: false,
-    logging: false,
-    extra: {
-      ssl: false,
-    },
-    entities: ['./src/models/*.ts'],
-    migrations: ['./src/database/migrations/*.ts'],
-    cli: {
-      migrationsDir: './src/database/migrations',
-    },
-  };
-  if (process.env.DATABASE_URL) {
-    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
-  } else {
-    // gets your default configuration
-    // you could get a specific config by name getConnectionOptions('production')
-    // or getConnectionOptions(process.env.NODE_ENV)
-    connectionOptions = await getConnectionOptions();
-  }
+// const getOptions = async () => {
+//   let connectionOptions: ConnectionOptions;
+//   connectionOptions = {
+//     type: 'postgres',
+//     synchronize: false,
+//     logging: false,
+//     extra: {
+//       ssl: false,
+//     },
+//     entities: ['./src/models/*.ts'],
+//     migrations: ['./src/database/migrations/*.ts'],
+//     cli: {
+//       migrationsDir: './src/database/migrations',
+//     },
+//   };
+//   if (process.env.DATABASE_URL) {
+//     Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+//   }
 
-  return connectionOptions;
-};
+//   connectionOptions = await getConnectionOptions();
+//   return connectionOptions;
+// };
+
+import { createConnection } from 'typeorm';
 
 const connect2Database = async (): Promise<void> => {
-  const typeormconfig = await getOptions();
-  await createConnection(typeormconfig);
+  await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+  });
 };
 
 connect2Database().then(async () => {
