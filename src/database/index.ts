6c5e2577fa6cc0487723eee30d64e617ae47f48a
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-14 04:33:37 -0300

    Procfile

--- src/database/index.ts
+++ src/database/index.ts
@@ -35,11 +35,13 @@
 // };
 
 import { createConnection } from 'typeorm';
+import Tool from '../models/Tool';
 
 const connect2Database = async (): Promise<void> => {
   await createConnection({
     type: 'postgres',
     url: process.env.DATABASE_URL,
+    entities: [Tool],
   });
 };
 
