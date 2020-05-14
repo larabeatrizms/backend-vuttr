3ba5cffcf4ff600fc87cdc60da6bfa47f5aa3069
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-14 01:21:09 -0300

    Procfile

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,22 @@
 import { createConnection } from 'typeorm';
+import * as PostgressConnectionStringParser from 'pg-connection-string';
 
-createConnection();
+// createConnection();
+
+const databaseUrl: string = process.env.DATABASE_URL;
+const connectionOptions = PostgressConnectionStringParser.parse(databaseUrl);
+const typeOrmOptions: PostgresConnectionOptions = {
+  type: 'postgres',
+  name: connectionOptions.name,
+  host: connectionOptions.host,
+  port: connectionOptions.port,
+  username: connectionOptions.username,
+  password: connectionOptions.password,
+  database: connectionOptions.database,
+  synchronize: true,
+  entities: ['target/entity/**/*.js'],
+  extra: {
+    ssl: true,
+  },
+};
+const connection = createConnection(typeOrmOptions);
