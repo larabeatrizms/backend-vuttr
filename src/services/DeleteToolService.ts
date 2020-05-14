4f5d39a45e26faaf14b9a89bd76e65619a35e5cb
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-14 02:36:34 -0300

    Procfile

--- src/services/DeleteToolService.ts
+++ src/services/DeleteToolService.ts
@@ -1,5 +1,4 @@
 import { getCustomRepository } from 'typeorm';
-import ToolRepository from '../repositories/ToolsRepository';
 import ToolsRepository from '../repositories/ToolsRepository';
 
 interface Request {
