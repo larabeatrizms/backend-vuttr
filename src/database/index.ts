3d4fae79d4085df5f430e7230551e8226b908af3
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-18 05:45:12 -0300

    Concluindo

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,26 +1,27 @@
-// import { createConnection } from 'typeorm';
-// createConnection();
-
 import { createConnection } from 'typeorm';
-import Tool from '../models/Tool';
 
-const connect2Database = async (): Promise<void> => {
-  await createConnection({
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    // synchronize: false,
-    migrationsRun: true,
-    // username: 'postgres',
-    // password: 'docker',
-    // database: 'vuttr',
-    // entities: [Tool],
-    // entities: [Tool],
-    entities: ['../models/*.ts'],
-    migrations: ['./migrations/*.ts'],
-    cli: {
-      migrationsDir: './migrations',
-    },
-  });
-};
+createConnection();
 
-export default connect2Database;
+// import { createConnection } from 'typeorm';
+// import Tool from '../models/Tool';
+
+// const connect2Database = async (): Promise<void> => {
+//   await createConnection({
+//     type: 'postgres',
+//     url: process.env.DATABASE_URL,
+//     // synchronize: false,
+//     migrationsRun: true,
+//     // username: 'postgres',
+//     // password: 'docker',
+//     // database: 'vuttr',
+//     // entities: [Tool],
+//     // entities: [Tool],
+//     entities: ['../models/*.ts'],
+//     migrations: ['./migrations/*.ts'],
+//     cli: {
+//       migrationsDir: './migrations',
+//     },
+//   });
+// };
+
+// export default connect2Database;
