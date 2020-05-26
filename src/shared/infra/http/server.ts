0c89cb407905ccc1dd4c2b3733c4fe0987a736f7
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-26 17:39:49 -0300

    :wrench: refactoring project to DDD methodology

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,5 +11,5 @@ import app from './app';
 const port = process.env.DATABASE_URL ? process.env.PORT : 3000;
 
 app.listen(port, () => {
-  console.log('🚀 Server started on port 3000!');
+  console.log('🚀 Server started!');
 });
